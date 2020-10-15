d9c884a56600bb621caeb8c09c78adb55e5c82f4
Samantha Csik <samanthacsik@gmail.com>
2020-10-14 17:01:08 -0700

    required packages

---
+++ code/00_libraries.R
@@ -0,0 +1,15 @@
+# title: Required Packages
+# author: "Sam Csik"
+# date created: "2020-10-14"
+# date edited: "2020-10-14"
+# R version: 3.6.3
+# input: NA
+# output: NA
+
+##############################
+# Load packages
+##############################
+
+library(tidyverse)
+library(httr)
+library(jsonlite)
