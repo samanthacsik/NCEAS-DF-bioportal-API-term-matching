cb3783a7a8cd0a4341e319ba5d3bfc1ec97eaf85
Samantha Csik <samanthacsik@gmail.com>
2020-10-14 17:01:33 -0700

    extract annotation matches to ARC terms

---
+++ code/01_ARC_BioPortal_annotation_matches.R
@@ -0,0 +1,92 @@
+# title: 
+# author: Sam Csik
+# date created: "2020-10-12"
+# date edited: "2020-10-14"
+# R version: 3.6.3
+# input: "data/ARC_vitalSigns_targetVars.csv"
+# output: "data/ARC_annotation_matches.csv"
+# resources: 
+  # BioOntology API Documentation: http://data.bioontology.org/documentation
+  # Best Practices for API Packages: https://cran.r-project.org/web/packages/httr/vignettes/api-packages.html
+  # httr Quickstart Guide: https://cran.r-project.org/web/packages/httr/vignettes/quickstart.html 
+  # Annotator Tab Help: http://bioportal.bioontology.org/help?pop=true#Annotator_Tab
+
+##########################################################################################
+# Summary
+##########################################################################################
+
+#....
+
+##########################################################################################
+# General setup
+##########################################################################################
+
+##############################
+# load packages
+##############################
+
+source(here::here("code", "00_libraries.R"))
+source(here::here("code", "00_functions.R"))
+
+##############################
+# load data
+##############################
+
+vitalSigns_targetVars <- read_csv(here::here("data", "ARC_vitalSigns_targetVars.csv")) 
+
+##########################################################################################
+# 1) Wrangle data
+##########################################################################################
+
+# add "+" in spaces between terms for API search
+vitalSigns_targetVars <- vitalSigns_targetVars %>% 
+  mutate(APItext = str_replace_all(ARC_term, " ", "+")) 
+
+# add cols to df
+vitalSigns_targetVars$num_annotation_matches <- "NA"
+vitalSigns_targetVars$valueURI <- "NA"
+
+##########################################################################################
+# 2) Query BioOntology for annotation matches for each of the ARC's Vital Signs and Target Variables
+##########################################################################################
+
+for(row in 1:nrow(vitalSigns_targetVars)){
+  
+  # ------------------------------------------------------------------
+  # define necessary objects
+  data <- vitalSigns_targetVars
+  term_index <- row 
+  search_term <- vitalSigns_targetVars$APItext[term_index] 
+  
+  print(term_index)
+  
+  # ------------------------------------------------------------------
+  # make API request and get parsed data
+  parsed <- make_bioontologyAPI_request(data, term_index)
+  
+  # ------------------------------------------------------------------
+  # append parsed data to original df; if no matches, return `NA`
+  tryCatch({
+
+    # first try
+    append_annotation_matches(num_matches, valueURI_matches)
+  },
+
+  # if error, print NA
+  error = function(e){
+    message(paste("There was an error for search term:", search_term))
+    message(paste("The error was:", e))
+    return(NA)
+  }
+  )
+  
+  print(paste("Done appending term number:", term_index))
+  print("--------------------")
+  
+  # ------------------------------------------------------------------
+  Sys.sleep(2)
+
+}
+
+# save as .csv
+write_csv(vitalSigns_targetVars, here::here("data", "ARC_annotation_matches.csv"))
